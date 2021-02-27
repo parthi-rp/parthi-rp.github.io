13383f74eb4ee8cd5c9910b43868ce4541be37f6
parthi_rp <rp_parthi@hotmail.com>
2021-02-27 13:02:45 +0530

    add new problem

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function isLandscape(width, height) {
-    console.log(width > height);
+//div by 3 => Fizz
+//div by 5 => Buzz
+//div by both => FizzBuzz
+//Not div by 3 or 5 => input
+//Not a number => 'Not a number'
+
+
+
+function fizzbuzz(input) {
+    if (typeof (input) == 'number') {
+        if (input % 3 == 0 && input % 5 == 0)
+            return 'FizzBuzz';
+        else if (input % 3 == 0)
+            return 'Fizz';
+        else if (input % 5 == 0)
+            return 'Buzz';
+        else
+            return input;
+    }
+    else
+        return NaN;
 }
 
-isLandscape(1920, 1080)
+const output = fizzbuzz('7');
+console.log(output);
