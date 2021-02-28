b306efbe78e387acb8e663cafd9a38f8f67a233c
parthi_rp <rp_parthi@hotmail.com>
2021-02-28 10:01:11 +0530

    add new problem

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-// speed limit => 70
-// 5 => 1 point
-// math.floor(1.3)
-// 12 points max => suspended
+showNumbers(10)
+
+function showNumbers(limit) {
 
 
+    for (let i = 0; i <= limit; i++) {
+        if (i % 2 == 0)
+            console.log(i + ' Even');
+        else
+            console.log(i + ' Odd');
 
-function checkSpeed(speed) {
-
-    let kmPerPoint = 5;
-    let speedLimit = 70;
-    let points = Math.floor((speed - speedLimit) / kmPerPoint);
-    let maxPoints = 12;
-
-
-    if (points == 0)
-        return 'ok';
-    else if (points <= maxPoints)
-        return points
-    else
-        return 'License suspended';
+    }
 }
-
-console.log(checkSpeed(77));
-
