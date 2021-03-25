c0aef523293f581beb06cdbabad14b0298ec9624
parthi_rp <rp_parthi@hotmail.com>
2021-03-25 18:35:10 +0530

    add prob in js

---
+++ blogs/blogs/problems.js
@@ -0,0 +1,31 @@
+
+let questions = {
+
+    1: '.Calculate power of the given number. Try using a)method as well as b)operator.',
+    2: '.Calculate the avg value of the numbers given in individual variables. Try using operator',
+    3: '.Object destructuring. Log all values of each prop using dot, Log all values of each prop using bracket, Log all values of each prop using dot and bracket',
+    4: '.Object destructuring. Bracket notation - not defined, Why object destructuring',
+    5: '.Object destructuring. aliases, new prop and default value',
+    6: '.Object destructuring. Use a variable to access the property in object, What is identifier',
+    7: '.sum of all elements in a given list. use loop and other method',
+    8: '.swap variables. a) use backup variable b)use es6',
+    9: '.display odd numbers below 10. a) for loops b) while loop',
+    10: document.getElementById('/blogs/blogs / assets / problems_assets / prob_6.png'),
+    11: '.Maximum of two numbers. a) use function',
+    12: '.landscape or portrait. a) dont use true or false explicitly b) dont use console in function',
+    13: '.FizzBuzz. a) div by 3 => fizz b) div by 5 => buzz c) div by 3 and 5 => fizzbuzz d) NaN e) other number => input',
+    14: '.check speed. a) 1 points for every 5 above 70 b) ok for 70 points c) max 12 points',
+    15: '.show odd and even upto limit. a) use ternary for console',
+    16: '.count truthy. a) for loop [in or of] b)return in for or if c) else statement'
+
+}
+
+
+
+for (var key in questions) {
+    var value = questions[key];
+    document.write(value);
+    document.write('<br>');
+    document.write('<br>');
+
+}
